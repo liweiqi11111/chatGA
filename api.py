b0f2bfc750aa5e530848cc669e502cc7bd0c0748
liweiqi11111 <1666341650@qq.com>
2023-08-24 10:27:31 +0800

    响应模型

--- api.py
+++ api.py
@@ -51,185 +51,6 @@ import pojo.Conversation as Conversation, pojo.Message as Message
 import db.dao as db
 from db.User import User
 
-# 创建用于JWT令牌签名的随机密钥：openssl rand -hex 32
-# JWT令牌签名算法：HS256，令牌过期时间：30分钟
-SECRET_KEY = "8bc1d6b8eb35aa64ed017d0c049bc34a3d035263b8d03fb4bb55693ad9c81549"
-ALGORITHM = "HS256"
-ACCESS_TOKEN_EXPIRE_MINUTES = 30
-
-credentials_exception = HTTPException(
-    status_code=status.HTTP_401_UNAUTHORIZED,
-    detail="Could not validate credentials",
-    headers={"WWW-Authenticate": "Bearer"},
-)
-
-
-# 定义令牌端点相应的Pydantic模型
-class Token(BaseModel):
-    access_token: str
-    token_type: str
-
-
-class TokenData(BaseModel):
-    username: Union[str, None] = None
-
-
-# 配置OAuth2
-oauth2_scheme = OAuth2PasswordBearer(tokenUrl="token")
-
-
-# 创建生成新的访问令牌的工具函数
-def create_access_token(data: dict, expires_delta: Union[timedelta, None] = None):
-    to_encode = data.copy()
-    if expires_delta:
-        expire = datetime.utcnow() + expires_delta
-    else:
-        expire = datetime.utcnow() + timedelta(minutes=ACCESS_TOKEN_EXPIRE_MINUTES)
-
-    to_encode.update({"exp": expire})
-    encode_jwt = jwt.encode(to_encode, SECRET_KEY, algorithm=ALGORITHM)
-    return encode_jwt
-
-
-# get_current_user()函数用于验证JWT令牌并返回当前用户
-async def get_current_user(token: str = Depends(oauth2_scheme)):
-    try:
-        payload = jwt.decode(token, SECRET_KEY, algorithms=[ALGORITHM])
-        username: str = payload.get("sub")
-        if username is None:
-            raise credentials_exception
-        token_data = TokenData(username=username)
-    except JWTError:
-        raise credentials_exception
-    userInDB = db.get_user(username=token_data.username)
-    if userInDB is None:
-        raise credentials_exception
-    return User(user_id=userInDB.user_id, username=userInDB.username)
-
-
-app = FastAPI()
-
-
-## 创建并返回真正的JWT访问令牌
-@app.post("/token", response_model=Token)
-async def login_for_access_token(form_data: OAuth2PasswordRequestForm = Depends()):
-    user = db.authenticate_user(
-        username=form_data.username, password=form_data.password
-    )
-    if not user:
-        raise HTTPException(
-            status_code=status.HTTP_401_UNAUTHORIZED,
-            detail="Incorrect username or password",
-            headers={"WWW-Authenticate": "Bearer"},
-        )
-    access_token_expires = timedelta(minutes=ACCESS_TOKEN_EXPIRE_MINUTES)
-    access_token = create_access_token(
-        data={"sub": user.username}, expires_delta=access_token_expires
-    )
-    return {"access_token": access_token, "token_type": "bearer"}
-
-
-# 注册接口
-@app.post("/register/")
-async def register(form_data: OAuth2PasswordRequestForm = Depends()):
-    # 查询用户是否已存在
-    if db.get_user(username=form_data.username):
-        raise HTTPException(status_code=400, detail="用户已存在")
-    # 创建用户
-    db.create_user(username=form_data.username, password=form_data.password)
-    return BaseResponse(200, "注册成功")
-
-
-##----- 需要进行登录拦截的接口 -----
-
-
-# 分页获取用户的所有会话，并根据更新时间进行排序 conversations?offset=0&limit=28&order=updated
-@app.get("/conversations/")
-async def get_conversations(
-    offset: int = 0,
-    limit: int = 28,
-    order: str = "updated",
-    current_user: User = Depends(get_current_user),
-):
-    # 登录拦截
-    if not current_user:
-        raise credentials_exception
-    conversations = db.get_conversations_by_user_id_with_orm(
-        user_id=current_user.user_id, offset=offset, limit=limit, order=order
-    )
-    return ConversationResponse(code=200, msg="获取成功", data=conversations)
-    # return {"code": 200, "msg": "获取成功", "data": conversations}
-
-
-# 根据会话id获取会话信息，并根据创建时间进行排序 conversation/1
-@app.get("/conversation/{conv_id}")
-async def get_messages(conv_id: int, current_user: User = Depends(get_current_user)):
-    # 登录拦截
-    if not current_user:
-        raise credentials_exception
-    messages = db.get_messages_by_conv_id_with_orm(conv_id=conv_id)
-    return MessageResponse(code=200, msg="获取成功", data=messages)
-    # return {"code": 200, "msg": "获取成功", "data": messages}
-
-
-# 创建会话
-@app.post("/conversation/")
-async def create_conversation(current_user: User = Depends(get_current_user)):
-    # 登录拦截
-    if not current_user:
-        raise credentials_exception
-    conv_id = db.create_conversation(user_id=current_user.user_id)
-    return BaseResponse(200, "更新成功, 创建会话conv_id:{}, 默认标题为'新的会话'".format(conv_id))
-    # return {"code": 200, "msg": "创建成功", "data": {"conv_id": conv_id}}
-
-
-# 更新会话
-@app.put("/conversation/{conv_id}")
-async def update_conversation(
-    conv_id: int, title: str, current_user: User = Depends(get_current_user)
-):
-    # 登录拦截
-    if not current_user:
-        raise credentials_exception
-    db.update_conversation(conv_id=conv_id, title=title)
-    return BaseResponse(200, "更新成功")
-
-
-# 删除会话
-@app.delete("/conversation/{conv_id}")
-async def delete_conversation(
-    conv_id: int, current_user: User = Depends(get_current_user)
-):
-    # 登录拦截
-    if not current_user:
-        raise credentials_exception
-    db.delete_conversation(conv_id=conv_id)
-    return BaseResponse(200, "删除成功")
-    # return {"code": 200, "msg": "删除成功"}
-
-
-# 创建消息
-@app.post("/message/")
-async def create_message(
-    conv_id: int,
-    role: str,
-    content: str,
-    content_type: str,
-    current_user: User = Depends(get_current_user),
-):
-    # 登录拦截
-    if not current_user:
-        raise credentials_exception
-    db.create_message(
-        conv_id=conv_id, role=role, content=content, content_type=content_type
-    )
-    return BaseResponse(200, "创建成功")
-
-
-###############################  NEW #################################
-###############################  NEW #################################
-
-
 class BaseResponse(BaseModel):
     code: int = pydantic.Field(200, description="HTTP status code")
     msg: str = pydantic.Field("success", description="HTTP status message")
@@ -339,6 +160,187 @@ class ChatMessage(BaseModel):
                 ],
             }
         }
+
+
+# 创建用于JWT令牌签名的随机密钥：openssl rand -hex 32
+# JWT令牌签名算法：HS256，令牌过期时间：30分钟
+SECRET_KEY = "8bc1d6b8eb35aa64ed017d0c049bc34a3d035263b8d03fb4bb55693ad9c81549"
+ALGORITHM = "HS256"
+ACCESS_TOKEN_EXPIRE_MINUTES = 30
+
+credentials_exception = HTTPException(
+    status_code=status.HTTP_401_UNAUTHORIZED,
+    detail="Could not validate credentials",
+    headers={"WWW-Authenticate": "Bearer"},
+)
+
+
+# 定义令牌端点相应的Pydantic模型
+class Token(BaseModel):
+    access_token: str
+    token_type: str
+
+
+class TokenData(BaseModel):
+    username: Union[str, None] = None
+
+
+# 配置OAuth2
+oauth2_scheme = OAuth2PasswordBearer(tokenUrl="token")
+
+
+# 创建生成新的访问令牌的工具函数
+def create_access_token(data: dict, expires_delta: Union[timedelta, None] = None):
+    to_encode = data.copy()
+    if expires_delta:
+        expire = datetime.utcnow() + expires_delta
+    else:
+        expire = datetime.utcnow() + timedelta(minutes=ACCESS_TOKEN_EXPIRE_MINUTES)
+
+    to_encode.update({"exp": expire})
+    encode_jwt = jwt.encode(to_encode, SECRET_KEY, algorithm=ALGORITHM)
+    return encode_jwt
+
+
+# get_current_user()函数用于验证JWT令牌并返回当前用户
+async def get_current_user(token: str = Depends(oauth2_scheme)):
+    try:
+        payload = jwt.decode(token, SECRET_KEY, algorithms=[ALGORITHM])
+        username: str = payload.get("sub")
+        if username is None:
+            raise credentials_exception
+        token_data = TokenData(username=username)
+    except JWTError:
+        raise credentials_exception
+    userInDB = db.get_user(username=token_data.username)
+    if userInDB is None:
+        raise credentials_exception
+    return User(user_id=userInDB.user_id, username=userInDB.username)
+
+
+app = FastAPI()
+
+
+## 创建并返回真正的JWT访问令牌
+@app.post("/token", response_model=Token)
+async def login_for_access_token(form_data: OAuth2PasswordRequestForm = Depends()):
+    user = db.authenticate_user(
+        username=form_data.username, password=form_data.password
+    )
+    if not user:
+        raise HTTPException(
+            status_code=status.HTTP_401_UNAUTHORIZED,
+            detail="Incorrect username or password",
+            headers={"WWW-Authenticate": "Bearer"},
+        )
+    access_token_expires = timedelta(minutes=ACCESS_TOKEN_EXPIRE_MINUTES)
+    access_token = create_access_token(
+        data={"sub": user.username}, expires_delta=access_token_expires
+    )
+    return {"access_token": access_token, "token_type": "bearer"}
+
+
+# 注册接口
+@app.post("/register/", response_model=BaseResponse)
+async def register(form_data: OAuth2PasswordRequestForm = Depends()):
+    # 查询用户是否已存在
+    if db.get_user(username=form_data.username):
+        raise HTTPException(status_code=400, detail="用户已存在")
+    # 创建用户
+    db.create_user(username=form_data.username, password=form_data.password)
+    return BaseResponse(200, "注册成功")
+
+
+##----- 需要进行登录拦截的接口 -----
+
+
+# 分页获取用户的所有会话，并根据更新时间进行排序 conversations?offset=0&limit=28&order=updated
+@app.get("/conversations/", response_model=ConversationResponse)
+async def get_conversations(
+    offset: int = 0,
+    limit: int = 28,
+    order: str = "updated",
+    current_user: User = Depends(get_current_user),
+):
+    # 登录拦截
+    if not current_user:
+        raise credentials_exception
+    conversations = db.get_conversations_by_user_id_with_orm(
+        user_id=current_user.user_id, offset=offset, limit=limit, order=order
+    )
+    return ConversationResponse(code=200, msg="获取成功", data=conversations)
+    # return {"code": 200, "msg": "获取成功", "data": conversations}
+
+
+# 根据会话id获取会话信息，并根据创建时间进行排序 conversation/1
+@app.get("/conversation/{conv_id}", response_model=MessageResponse)
+async def get_messages(conv_id: int, current_user: User = Depends(get_current_user)):
+    # 登录拦截
+    if not current_user:
+        raise credentials_exception
+    messages = db.get_messages_by_conv_id_with_orm(conv_id=conv_id)
+    return MessageResponse(code=200, msg="获取成功", data=messages)
+    # return {"code": 200, "msg": "获取成功", "data": messages}
+
+
+# 创建会话
+@app.post("/conversation/", response_model=BaseResponse)
+async def create_conversation(current_user: User = Depends(get_current_user)):
+    # 登录拦截
+    if not current_user:
+        raise credentials_exception
+    conv_id = db.create_conversation(user_id=current_user.user_id)
+    return BaseResponse(200, "更新成功, 创建会话conv_id:{}, 默认标题为'新的会话'".format(conv_id))
+    # return {"code": 200, "msg": "创建成功", "data": {"conv_id": conv_id}}
+
+
+# 更新会话
+@app.put("/conversation/{conv_id}", response_model=BaseResponse)
+async def update_conversation(
+    conv_id: int, title: str, current_user: User = Depends(get_current_user)
+):
+    # 登录拦截
+    if not current_user:
+        raise credentials_exception
+    db.update_conversation(conv_id=conv_id, title=title)
+    return BaseResponse(200, "更新成功")
+
+
+# 删除会话
+@app.delete("/conversation/{conv_id}", response_model=BaseResponse)
+async def delete_conversation(
+    conv_id: int, current_user: User = Depends(get_current_user)
+):
+    # 登录拦截
+    if not current_user:
+        raise credentials_exception
+    db.delete_conversation(conv_id=conv_id)
+    return BaseResponse(200, "删除成功")
+    # return {"code": 200, "msg": "删除成功"}
+
+
+# 创建消息
+@app.post("/message/", response_model=BaseResponse)
+async def create_message(
+    conv_id: int,
+    role: str,
+    content: str,
+    content_type: str,
+    current_user: User = Depends(get_current_user),
+):
+    # 登录拦截
+    if not current_user:
+        raise credentials_exception
+    db.create_message(
+        conv_id=conv_id, role=role, content=content, content_type=content_type
+    )
+    return BaseResponse(200, "创建成功")
+
+
+###############################  NEW #################################
+###############################  NEW #################################
+
+
 
 
 def get_kb_path(local_doc_id: str):
