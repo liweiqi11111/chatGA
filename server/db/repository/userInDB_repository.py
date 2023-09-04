755878932bbdff82999e76225cffdda032129e93
liweiqi11111 <1666341650@qq.com>
2023-09-04 23:32:12 +0800

    修改bug

--- server/db/repository/userInDB_repository.py
+++ server/db/repository/userInDB_repository.py
@@ -18,7 +18,7 @@ def authenticate_user(session, username: str, password: str):
     user = get_user(username)
     if not user:
         return False
-    if not pwd_context.verify(password, user.hashed_password):
+    if not pwd_context.verify(password, user.password):
         return False
     return True
 
