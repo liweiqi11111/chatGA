eeeee24aaf531e20620846563363f2f412ac1e17
liweiqi11111 <1666341650@qq.com>
2023-09-04 23:24:30 +0800

    修改bug

--- server/db/models/userInDB_model.py
+++ server/db/models/userInDB_model.py
@@ -10,7 +10,7 @@ class UserInDBModel(Base):
     __tablename__ = 't_user'
     user_id = Column(Integer, primary_key=True, autoincrement=True, comment='用户ID')
     username = Column(String, comment='用户名')
-    hashed_password = Column(String, comment='加密后密码')
+    password = Column(String, comment='加密后密码')
     create_time = Column(DateTime, default=func.now(), comment='创建时间')
 
     def __repr__(self):
